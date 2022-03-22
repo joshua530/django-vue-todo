3130839b547cb70db6bef95c5e596804ce164ba5
joshua530 <joshuaomari5@gmail.com>
2022-03-23 00:16:42 +0300

    use postgresql dbms

--- backend/src/todo_app/settings.py
+++ backend/src/todo_app/settings.py
@@ -59,14 +59,15 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '<blog name>',
+        'USER': '<enter user>',
+        'PASSWORD': '<enter password>'
     }
 }
+
 
 
 # Password validation
